ed06f1a0d290815ff7b4f61dbc4c14f791c922f3
EpsilonNikita <epsilon840@gmail.com>
2016-07-18 16:11:50 +0300

    create user edit functional (Angular2)

--- src/main/java/com/nevorinc/myowncms/config/initial/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.nevorinc.myowncms.config.initial;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
--- src/main/webapp/scripts/admin/components/user_form/user_form.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'user-form',
-    template: `<div>UserForm</div>`
-})
-export class User_FormComponent {}
